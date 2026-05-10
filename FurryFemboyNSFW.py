3faf5162b89619511f00fdd0664faad1c68239e3
LidF1x <135136616+sz3333@users.noreply.github.com>
2026-05-10 16:09:11 +0300

    Update FurryFemboyNSFW.py

--- FurryFemboyNSFW.py
+++ FurryFemboyNSFW.py
@@ -34,7 +34,7 @@ class YiffScrollerMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "channels",
-                ["@FurryFemboysPlace,@gexfor20, @FemboysSpanish, @BadFurryFemboy, @paws_qq,@furry_yaoi_arts"],
+                ["FurryFemboysPlace,gexfor20, FemboysSpanish,BadFurryFemboy, paws_qq,furry_yaoi_arts"],
                 "Список каналов для загрузки",
                 validator=loader.validators.Series()
             ),
